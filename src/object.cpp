6a825052a0da9c2a9754f0e0b43f0e5aa8118e9e
Indominus_Rex <singtoppy@gmail.com>
2026-05-06 23:18:27 +0700

    fix collision one more time

--- src/object.cpp
+++ src/object.cpp
@@ -1,4 +1,4 @@
 #include "object.hpp"
 
 Object::Object(float x, float y, float w, float h, bool canCollide) : 
-    x(x), y(y), w(w), h(h), rect({x, y, w, h}), canCollide(canCollide) {}
+    x(x), y(y), w(w), h(h), rect({x, y, w, h}), canCollide(canCollide) {}   
