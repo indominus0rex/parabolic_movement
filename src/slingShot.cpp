c78bb5d0a1a5958f9860eff3d2134e94ba7d093e
Indominus_Rex <singtoppy@gmail.com>
2026-05-11 15:06:39 +0700

    draw slingshot

--- src/slingShot.cpp
+++ src/slingShot.cpp
@@ -11,26 +11,31 @@
 Slingshot::Slingshot() : mouseDown(false) {}
 
 void Slingshot::draw(SDL_Renderer* renderer) {
-    if (startPosition == glm::vec2(-1, -1) || endPosition == glm::vec2(-1, -1))
+    if (!mouseDown || startPosition == defaultMousePosition || endPosition == defaultMousePosition)
         return;
+
+    std::cout << startPosition.x << ' ' << startPosition.y << ' ' << endPosition.x << ' ' << endPosition.y << '\n';
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderLine(renderer, startPosition.x, startPosition.y, endPosition.x, endPosition.y);
 }
 
-void Slingshot::handleEvents(SDL_Event& event, std::vector<std::unique_ptr<Object>>& object, Window* window) {
+void Slingshot::handleEvents(Window* window, SDL_Event& event, std::vector<std::unique_ptr<Object>>& object) {
     if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
         mouseDown = true;
         startPosition = getMousePosition(window, event);
-    }    
+        endPosition = getMousePosition(window, event);
+    }
+    
+    if (event.type == SDL_EVENT_MOUSE_MOTION && mouseDown) {
+        endPosition = getMousePosition(window, event);
+    }
 
     if (event.type == SDL_EVENT_MOUSE_BUTTON_UP) {
         mouseDown = false;
+        // startPosition = defaultMousePosition;
+        // endPosition = defaultMousePosition;
     }
 }
 
-void Slingshot::update(Window* window, float deltaTime) {
-    if (mouseDown) {
-
-    }
-}
+void Slingshot::update(Window* window, float deltaTime) {}
--- src/slingshot.hpp
+++ src/slingshot.hpp
@@ -15,6 +15,8 @@ protected:
     glm::vec2 endPosition;
     bool mouseDown;
 
+    glm::vec2 defaultMousePosition = glm::vec2(-1, -1);
+
     void createNewParticle(std::vector<std::unique_ptr<Object>> &objects) {
         Particle particle(startPosition.x, startPosition.y, 50, 50, glm::distance(startPosition.x, endPosition.x), glm::distance(startPosition.y, endPosition.y), 1, SDL_Color{255, 255, 255, 255});
         objects.push_back(std::make_unique<Particle>(particle));
@@ -26,5 +28,5 @@ public:
 
     void update(Window* window, float deltaTime) override;
     void draw(SDL_Renderer* renderer);
-    void handleEvents(SDL_Event& event, std::vector<std::unique_ptr<Object>>& objects, Window* window);
+    void handleEvents(Window* window, SDL_Event& event, std::vector<std::unique_ptr<Object>>& objects);
 };
