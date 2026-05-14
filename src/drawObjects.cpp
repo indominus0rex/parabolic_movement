61c63b03a58388b6d55e33f3e2aa44d508851bdf
Indominus_Rex <singtoppy@gmail.com>
2026-05-14 22:19:01 +0700

    add drawObject

---
+++ src/drawObjects.cpp
@@ -0,0 +1,52 @@
+#include <SDL3/SDL.h>
+#include <vector>
+#include <memory>
+#include <glm/glm.hpp>
+
+#include "drawObjects.hpp"
+#include "window.hpp"
+#include "object.hpp"
+
+void DrawObjects::draw(SDL_Renderer* renderer) {
+
+}  
+
+void DrawObjects::handleEvents(Window* window, SDL_Event& event) {
+    if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN && currentMode == InputMode::DRAW) {
+        mouseDown = true;
+        std::visit(Overload{
+            [&](DrawObjectRectData& rect) {
+                rect.startPosition = this->getMousePosition(window, event);
+            },
+            [&](DrawObjectLineData& line) {
+                line.previousMousePosition = this->getMousePosition(window, event);
+                line.vertices.push_back(this->getMousePosition(window, event));
+            }
+        }, drawData);
+    }
+
+    if (event.type == SDL_EVENT_MOUSE_MOTION && mouseDown) {
+        std::visit(Overload{
+            [&](DrawObjectRectData& rect) {
+                rect.endPosition = getMousePosition(window, event);
+            },
+            [&](DrawObjectLineData& line) {
+                line.vertices.push_back(getMousePosition(window, event));
+                line.previousMousePosition = getMousePosition(window, event);
+            }
+        }, drawData);
+    }
+
+    if (event.type == SDL_EVENT_MOUSE_BUTTON_UP && mouseDown) {
+        mouseDown = false;
+        std::visit(Overload{
+            [&](DrawObjectRectData& rect) {
+                SDL_FRect newRect = rect.getRect();
+                SDL_RenderFillRect(window->getRenderer(), &newRect);
+            },
+            [&](DrawObjectLineData& line) {
+                SDL_RenderLines(window->getRenderer(), (SDL_FPoint* ) line.vertices.data(), (int) line.vertices.size());
+            }
+        }, drawData);
+    }
+}
---
+++ src/drawObjects.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <SDL3/SDL.h>
+#include <variant>
+#include <vector>
+#include <memory>
+
+#include "window.hpp"
+#include "object.hpp"
+#include "interaction.hpp"
+
+enum InputMode {
+    SHOOT,
+    DRAW
+};
+
+struct DrawObjectRectData {
+    glm::vec2 startPosition;
+    glm::vec2 endPosition;
+    SDL_Color color;
+
+    SDL_FRect getRect() const {
+        return {
+            startPosition.x,
+            startPosition.y,
+            startPosition.x + endPosition.x,
+            startPosition.y + endPosition.y
+        };
+    }
+};
+
+struct DrawObjectLineData {
+    glm::vec2 previousMousePosition;
+    std::vector<glm::vec2> vertices;
+    SDL_Color color;
+};
+
+class DrawObjects : public Interaction {
+protected:
+
+    InputMode currentMode = InputMode::SHOOT;
+    bool mouseDown;
+    std::variant<DrawObjectRectData, DrawObjectLineData> drawData;
+
+public:
+
+    void draw(SDL_Renderer* renderer) override;
+    void handleEvents(Window* window, SDL_Event& event) override;
+
+};
